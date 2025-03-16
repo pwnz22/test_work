8d0c81437bd9af099c61c797d4c8836851e1d9cb
Valijon <parliament888@gmail.com>
2025-03-17 00:07:43 +0500

    feat: добавил preloader при поиске городов

--- wp-content/themes/storefront-child/page-cities-weather.php
+++ wp-content/themes/storefront-child/page-cities-weather.php
@@ -13,8 +13,17 @@ get_header(); ?>
     <!-- Custom Hook: Перед таблицей -->
     <?php do_action('before_cities_weather_table'); ?>
 
-    <!-- Поле поиска -->
-    <input class="w-full" type="text" id="search-city" placeholder="Введите название города..." />
+    <div class="relative">
+      <!-- Поле поиска -->
+      <input class="w-full" type="text" id="search-city" placeholder="Введите название города..." />
+      <!-- Прелоадер (изначально скрыт) -->
+      <button id="preloader" class="hidden inline-flex absolute top-1/2 -translate-y-1/2 right-1">
+        <svg class=" animate-spin -ml-1 mr-3 h-5 w-5 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+          <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+      </button>
+    </div>
 
     <!-- Таблица с городами -->
     <table id="cities-weather-table">
@@ -79,7 +88,11 @@ get_header(); ?>
 
     // Функция поиска городов
     function searchCities() {
-      let query = $("#search-city").val().trim();
+      var query = $("#search-city").val().trim();
+      var preloader = $("#preloader");
+
+      // Показываем прелоадер
+      preloader.removeClass("hidden");
 
       $.ajax({
         url: "<?php echo admin_url('admin-ajax.php'); ?>",
@@ -90,6 +103,10 @@ get_header(); ?>
         },
         success: function(response) {
           $("#cities-weather-table tbody").html(response);
+        },
+        complete: function() {
+          // Скрываем прелоадер после завершения запроса
+          preloader.addClass("hidden");
         }
       });
     }
